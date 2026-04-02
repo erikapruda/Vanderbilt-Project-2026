3a689db148a0984014f08135564764e302d4e9b6
Cel4h <cel4h@mtmail.mtsu.edu>
2026-04-02 14:50:47 -0500

    Created countdown on start. Added public pixel font for in game and UI elements.

--- Assets/Scripts/TestLoader.cs
+++ Assets/Scripts/TestLoader.cs
@@ -7,20 +7,24 @@ public class TestLoader : MonoBehaviour
     public GameObject emotionPrefab;
     public GameObject arithmeticPrefab;
 
-    void Awake()
+    public void ActivateSelectedMode()
     {
+        if (stroopPrefab != null) stroopPrefab.SetActive(false);
+        if (emotionPrefab != null) emotionPrefab.SetActive(false);
+        if (arithmeticPrefab != null) arithmeticPrefab.SetActive(false);
+
         string selectedMode = PlayerPrefs.GetString("GameModifier", "");
 
         switch (selectedMode)
         {
             case "Stroop":
-                if (stroopPrefab != null) stroopPrefab.SetActive(true);
+                stroopPrefab.SetActive(true);
                 break;
             case "Emotion":
-                if (emotionPrefab != null) emotionPrefab.SetActive(true);
+                emotionPrefab.SetActive(true);
                 break;
             case "Arithmetic":
-                if (arithmeticPrefab != null) arithmeticPrefab.SetActive(true);
+                arithmeticPrefab.SetActive(true);
                 break;
             default:
                 Debug.LogWarning("Nothing has been selected.");
